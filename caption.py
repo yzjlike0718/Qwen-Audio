050139298fb146c80b45db4f45c0f5c69617be52
likehan <likehan@modelbest.cn>
2025-04-08 10:55:41 +0800

    feat:SER

--- caption.py
+++ caption.py
@@ -1,24 +1,28 @@
-import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
-import os
-
-# 如果您希望结果可复现，可以设置随机数种子。
+import torch
 torch.manual_seed(1234)
 
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-Audio-Chat", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("Qwen-Audio", trust_remote_code=True)
 
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-Audio-Chat", device_map="cuda", trust_remote_code=True).eval()
-model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-Audio-Chat", trust_remote_code=True)
+# use bf16
+# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-Audio", device_map="auto", trust_remote_code=True, bf16=True).eval()
+# use fp16
+# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-Audio", device_map="auto", trust_remote_code=True, fp16=True).eval()
+# use cpu only
+model = AutoModelForCausalLM.from_pretrained("Qwen-Audio", device_map="cpu", trust_remote_code=True).eval()
+# use cuda device
+# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-Audio", device_map="cuda", trust_remote_code=True).eval()
 
-meld_dir = "/local/likh/Qwen2-Audio/data/meld"
-
-for audio_name in os.listdir(meld_dir):
-    audio_path = os.path.join(meld_dir, audio_name)
-
-    query = tokenizer.from_list_format([
-        {'audio': audio_path},
-        {'text': 'what is that sound?'},
-    ])
-    response, history = model.chat(tokenizer, query=query, history=None)
-    print(response)
+# Specify hyperparameters for generation (No need to do this if you are using transformers>4.32.0)
+# model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-Audio", trust_remote_code=True)
+audio_url = "assets/audio/1272-128104-0000.flac"
+sp_prompt = "<|startoftranscription|><|en|><|transcribe|><|en|><|notimestamps|><|wo_itn|>"
+query = f"<audio>{audio_url}</audio>{sp_prompt}"
+audio_info = tokenizer.process_audio(query)
+inputs = tokenizer(query, return_tensors='pt', audio_info=audio_info)
+inputs = inputs.to(model.device)
+pred = model.generate(**inputs, audio_info=audio_info)
+response = tokenizer.decode(pred.cpu()[0], skip_special_tokens=False,audio_info=audio_info)
+print(response)
+# <audio>assets/audio/1272-128104-0000.flac</audio><|startoftranscription|><|en|><|transcribe|><|en|><|notimestamps|><|wo_itn|>mister quilting is the apostle of the middle classes and we are glad to welcome his gospel<|endoftext|>
---
+++ run_SER.sh
@@ -0,0 +1,17 @@
+checkpoint="Qwen-Audio"
+data_config_path="data_config.json"
+output_path="/user/likehan/output_qwen_audio"
+mkdir -p $output_path
+
+NPROC_PER_NODE=4 # Set the number of GPUs per node
+NODES=1 # Set the number of nodes
+
+python -m torch.distributed.launch --use-env \
+    --nnodes=$NODES \
+    --nproc_per_node=$NPROC_PER_NODE \
+    SER.py \
+    --checkpoint $checkpoint \
+    --data_config_path $data_config_path \
+    --output_path $output_path \
+    --batch-size 8 \
+    --num-workers 2
